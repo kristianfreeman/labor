0bc404b536b7a53cb4f98c2463d4310fa8762e68
Kristian Freeman <kristian@kristianfreeman.com>
2020-02-11 12:55:09 -0600

    potential jwt verification

--- packages/access/index.js
+++ packages/access/index.js
@@ -1,6 +1,7 @@
 import cookie from "cookie";
+import jose from "jose";
 
-const DEFAULT_CONFIG = { debug: false };
+const DEFAULT_CONFIG = { debug: false, verification: {} };
 
 export default class AccessAuthorizer {
   constructor(config = DEFAULT_CONFIG) {
@@ -16,15 +17,32 @@ export default class AccessAuthorizer {
   try(evt) {
     this.log("Trying to run middleware");
     this.log(JSON.stringify(evt));
-    return new Promise((resolve, reject) => {
+
+    return new Promise(async (resolve, reject) => {
       const cookiesHeader = evt.request.headers.get("Cookie") || "";
       const cookies = cookie.parse(cookiesHeader);
-      if (cookies["CF_Authorization"]) {
-        this.log("Found header, resolving promise");
+      this.log(`Parsed cookies: ${cookies}`);
+
+      try {
+        const authCookie = cookies["CF_Authorization"];
+        if (!authCookie)
+          throw new Error("CF_Authorization wasn't found in cookie");
+
+        this.log(`Found CF_Authorization cookie`);
+
+        const certificate_url = this._config.verification.certificateUrl;
+        this.log(`Requesting certs from ${certificate_url}`);
+        const resp = await fetch(certificate_url);
+        const { keys } = await resp.json();
+        this.log(`Requested verification keys, received ${keys.length}`);
+
+        this.log(`Verifying...`);
+        const keyStore = jose.JWKS.asKeyStore(keys);
+        jose.JWT.verify(authCookie, keyStore);
         return resolve({ authorized: true, event: evt });
-      } else {
+      } catch (err) {
         this.log("Unable to authorize, rejecting promise");
-        reject(new Error("Unable to authorize"));
+        reject(err);
       }
     });
   }
